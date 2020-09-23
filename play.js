aa11c86883ce8eaa311cdb299b0c01ab7b726afd
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-23 15:30:57 +0000

    added base logic for client file

--- play.js
+++ play.js
@@ -3,5 +3,16 @@ const require = require('net');
 // establishes connection with game server
 
 const connect = function () {
-  const conn = net.
+  const conn = net.createConnection({
+    host:'<IP ADDRESS HERE>',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
 }
+
+console.log('Connecting ...');
+connect();
