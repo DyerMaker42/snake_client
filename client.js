b25d7d774f0ca50ae3e2da3f4a6d0412e7c33b6d
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-23 17:10:30 +0000

    added logic to move snake

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ const net = require('net');
 const connect = function () {
   //where we are connecting
   const conn = net.createConnection({
-    host:'localhost',
+    host: 'localhost',
     port: 50541
   });
 
@@ -13,14 +13,30 @@ const connect = function () {
   conn.setEncoding('utf8');
   //what to do when it recieves data
   conn.on('data', (data) => {
-    console.log("server says: ",data);
+    console.log("server says: ", data);
   });
   //what to do when it connects
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name:DAD');
   })
-
+  //move snake up on connect
+  conn.on('connect', () => {
+    //moves snake up
+    conn.write('Move: up')
+    // for loop that moves snek up and left
+    for (let time = 0; time < 90; time++) {
+      if (time % 2 === 0) {
+        setTimeout(() => {
+          conn.write("Move: left")
+        }, time*1000)
+      } else {
+        setTimeout(() => {
+          conn.write("Move: up")
+        }, time*500)
+      }
+    }
+  });
   return conn;
 }
 //exporting our connect function
