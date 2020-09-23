7f5217424619e2473b1834e9fe4435a7a492bede
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-23 16:13:53 +0000

    moved connect function into client js file

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+// establishes connection with game server
+
+const connect = function () {
+  const conn = net.createConnection({
+    host:'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log("server says: ",data);
+  });
+
+  return conn;
+}
+
+module.exports = connect;
