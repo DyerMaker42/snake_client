fe71204213f2f11ae853978a33a31d6dbd2a6596
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-23 20:13:49 +0000

    created new input js file to hold setup input and handleuserinput functions

--- play.js
+++ play.js
@@ -2,21 +2,7 @@ const  connect = require('./client');
 
 //setup user interface
 
-const handleUserInput = function(data){
-  if(data === '\u0003'){
-    process.exit();
-  }
-  //stdin.write(data);
-}
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  //handleUserInput();
-  return stdin;
-}
+
 
 console.log('Connecting ...');
 connect();
