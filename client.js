a103c0ff6d75dcaaeb384b16a9c66ca2132a2b00
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-23 16:27:47 +0000

    added ability of player to send 3-digit name, added comments

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ const net = require('net');
 // establishes connection with game server
 
 const connect = function () {
+  //where we are connecting
   const conn = net.createConnection({
     host:'localhost',
     port: 50541
@@ -10,16 +11,17 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
+  //what to do when it recieves data
   conn.on('data', (data) => {
     console.log("server says: ",data);
   });
-
+  //what to do when it connects
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
+    conn.write('Name:DAD');
   })
 
   return conn;
 }
-
+//exporting our connect function
 module.exports = connect;
