49a091c08019772dd5e7d1442f5d676c7ba47444
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-23 16:06:54 +0000

    added logic to handle incoming data

--- play.js
+++ play.js
@@ -1,18 +1,24 @@
-const require = require('net');
+const net = require('net');
 
 // establishes connection with game server
 
 const connect = function () {
   const conn = net.createConnection({
-    host:'<IP ADDRESS HERE>',
+    host:'localhost',
     port: 50541
   });
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('data', (data) => {
+    console.log("server says: ",data);
+  });
+
   return conn;
 }
 
 console.log('Connecting ...');
 connect();
+
+
